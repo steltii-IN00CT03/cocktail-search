7d7792737fdf8b692a25d8de022f6a9405daaae3
Juhani Koski <juhkukoskii@gmail.com>
2022-10-25 20:39:58 +0300

    1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>Coctail search</h1>
+        {/* <input type="text" value={searchText} onChange={ e => setSearchText(e.target.value) }/>
+        <button onClick={ e => searchForPlayer(e) }>Search for player</button> */}
+      </div>
     </div>
   );
 }
